e3c92e93c5fdac12f038a3d39f77cadf184dc345
JLabrum <jlabrum@tamu.edu>
2019-04-08 13:34:30 -0500

    Added questions.txt and adjusted code look

--- javafit/src/FileAccess.java
+++ javafit/src/FileAccess.java
@@ -1,10 +1,20 @@
 // Write code to load the workouts from the provided workouts.csv file. The function should return a Workouts object.
 
-public class FileAccess {
-  
-  public static Workouts loadWorkouts() {
-    // What is a try/catch block and why do we need one?
-    // What is an exception?
-  }
-
+public class FileAccess
+{
+	public static Workouts loadWorkouts()
+	{
+		// What is a try/catch block and why do we need one?
+		// What is an exception?
+		Workouts workouts = new Workouts();
+		try
+		{
+			
+		}
+		catch error
+		{
+			
+		}
+		return workouts;
+	}
 }
